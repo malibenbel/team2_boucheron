f76a20b623fc60b0b7cd39c2a1676f754e134c2d
gianluca-filesi <gianluca.filesi@mail.com>
2025-02-18 18:46:04 +0100

    Created .envrc

--- luxury_project/luxury_project/params.py
+++ luxury_project/luxury_project/params.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 
+load_dotenv()
 # Load environment variables load_dotenv()
 GOOGLE_CREDENTIALS = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
 PROJECT_ID = os.getenv("PROJECT_ID")
