14a8172e9ef3dc7b4e6043aa634031ffeca31164
gianluca-filesi <gianluca.filesi@mail.com>
2025-02-18 15:22:26 +0100

    Created data, params, clean file

---
+++ luxury_project/luxury_project/ml_logic/data.py
@@ -0,0 +1,12 @@
+from google.cloud import bigquery
+import pandas as pd
+import os
+from luxury_project.params import GOOGLE_CREDENTIALS, PROJECT_ID
+
+
+# Authenticate & initialize BigQuery client 
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = GOOGLE_CREDENTIALS client = bigquery.Client()
+
+def load_data(query):
+    df = client.query(query).to_dataframe() print(f"Loaded {df.shape[0]} rows.") 
+    return df
