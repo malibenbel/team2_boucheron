25dbebea04443e8b2007e2bc5ae1103954a4b2b4
gianluca-filesi <gianluca.filesi@mail.com>
2025-02-18 21:33:31 +0100

    Main upadated

--- luxury_project/luxury_project/interface/main.py
+++ luxury_project/luxury_project/interface/main.py
@@ -5,7 +5,9 @@ from luxury_project.ml_logic.registry import save_data
 df_sales = load_data("SELECT * FROM `still-dynamics-451213-b9.Price_Monitoring.Sales`")
 df_price = load_data("SELECT * FROM `still-dynamics-451213-b9.Price_Monitoring.Price`")
 
-df_sales['EUR'] = convert_to_eur(df_sale[''])
+df_sales['total_sales_EUR'] = convert_to_eur(df_sale['currency'], df_sale['total_sales'])
+df_sales['prices_EUR'] = convert_to_eur(df_sale['currency'], df_sale['price'])
 
 save_data(df_sales, "Sales_EUR")
-
+save_data(df_price, "Price_EUR")
+print("Data saved successfully")
