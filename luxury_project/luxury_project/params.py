7aec0617bf213b0c813bba5a9b114d8267a256c3
gianluca-filesi <gianluca.filesi@mail.com>
2025-02-18 16:48:14 +0100

    Add tables in params.py

--- luxury_project/luxury_project/params.py
+++ luxury_project/luxury_project/params.py
@@ -4,5 +4,6 @@ from dotenv import load_dotenv
 # Load environment variables load_dotenv()
 GOOGLE_CREDENTIALS = os.getenv("GOOGLE_APPLICATION_CREDENTIALS") 
 PROJECT_ID = os.getenv("PROJECT_ID")
-DATASET_ID = os.getenv("DATASET_ID")
+PRICE_DATASET_ID = os.getenv("PRICE_DATASET_ID")
+SALES_DATASET_ID = os.getenv("SALES_DATASET_ID")
 CURRENCY_API = os.getenv("CURRENCY_API_KEY")
