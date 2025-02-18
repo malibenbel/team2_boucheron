b15803602f9a23ffd17cbd462fbda454d47d1982
gianluca-filesi <gianluca.filesi@mail.com>
2025-02-18 22:01:45 +0100

    commit

--- luxury_project/luxury_project/interface/main.py
+++ luxury_project/luxury_project/interface/main.py
@@ -6,21 +6,12 @@ import pandas as pd
 df_sales = load_data("SELECT * FROM `still-dynamics-451213-b9.Price_Monitoring.Sales`")
 df_price = load_data("SELECT * FROM `still-dynamics-451213-b9.Price_Monitoring.Price`")
 
-<<<<<<< HEAD
-df_sales['total_sales_EUR'] = convert_to_eur(df_sale['currency'], df_sale['total_sales'])
-df_sales['prices_EUR'] = convert_to_eur(df_sale['currency'], df_sale['price'])
-
-save_data(df_sales, "Sales_EUR")
-save_data(df_price, "Price_EUR")
-=======
 df_price["price"] = pd.to_numeric(df_price["price"], errors="coerce")
 
-df_sales["total_sales_EUR"] = convert_to_eur(
-    df_sales["currency"], df_sales["total_sales"]
-)
+df_sales["total_sales_EUR"] = convert_to_eur(df_sales["currency"], df_sales["total_sales"])
 df_price["prices_EUR"] = convert_to_eur(df_price["currency"], df_price["price"])
 
 save_data(df_sales, "SalesEUR")
 save_data(df_price, "PriceEUR")
->>>>>>> 452e924 (Works)
+
 print("Data saved successfully")
