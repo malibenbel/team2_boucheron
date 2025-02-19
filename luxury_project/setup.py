996bbf21f34631796554a1d0d2e6c53c917d6a57
gianluca-filesi <gianluca.filesi@mail.com>
2025-02-19 16:06:32 +0100

    New requirements

--- luxury_project/setup.py
+++ luxury_project/setup.py
@@ -6,9 +6,11 @@ with open("requirements.txt") as f:
     content = f.readlines()
 requirements = [x.strip() for x in content]
 
-setup(
-    name="luxury_project",
-    description="package description",
+setup(name="luxury_project",
+    version="1.0.0",
+    author="Team2",
+    author_email="gianluca.filesi@edhec.com",
+    description="Luxury-project for Boucheron",
     packages=find_packages(),  # NEW: find packages automatically
     install_requires=requirements,
 )
