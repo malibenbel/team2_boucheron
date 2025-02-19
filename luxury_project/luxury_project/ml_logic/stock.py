380713eb103d39f472314cbcc5435f9a84acd7b5
gianluca-filesi <gianluca.filesi@mail.com>
2025-02-19 10:25:41 +0100

    Stock function added

--- luxury_project/luxury_project/ml_logic/stock.py
+++ luxury_project/luxury_project/ml_logic/stock.py
@@ -1,1 +1,8 @@
 import yfinance as yf
+import pandas as pd
+
+def get_stock_data(ticker_symbol='KER.PA'):
+    data = yf.download(ticker_symbol, period='5d', interval='1d')
+    data = pd.DataFrame(data)
+    return data
+
